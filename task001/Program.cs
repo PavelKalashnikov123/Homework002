989baee03cde60f14f3f58cc9deaa18d8368e260
PavelKalashnikov123 <pavel-kalashnikov@mail.ru>
2022-08-11 22:34:17 +0300

    task1

---
+++ task001/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выход показывает
+//вторую цифру этого числа
+//456->5
+//782->8
+//918->1
+
+Console.WriteLine("Введите трезначное чило");
+string? threeDigitNumber = Console.ReadLine();
+var number = Convert.ToString(threeDigitNumber!);
+Console.WriteLine("Вторая цифра этого числа " +number[1]);
